eccc79d1193a2e1220fb0f7553db8409b1108a48
Baoli Zhai <horidream@gmail.com>
2017-08-10 08:06:44 +0800

    udpate

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,35 @@
 var webpack = require('webpack');
 var path = require('path');
-
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    entry: {
+        bundle: './src/index.js',
+        vendor: ["faker", "lodash", "react", "react-dom", "react-input-range", "react-redux", "react-router", "redux", "redux-form", "redux-thunk"]
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[chunkhash:8].js'
+    },
+    module:{
+        rules:[
+        {
+            use:'babel-loader',
+            test:/\.js$/,
+            exclude:/node_modules/
+        }
+        ,{
+            use:['style-loader','css-loader'],
+            test:/\.css$/
+        }
+        
+      ]},
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ["vendor",'manifest']
+        }),
+        new HTMLWebpackPlugin({
+            inlineSource:'.js$',
+            template: './src/index.html'
+        })
+    ]
 };
